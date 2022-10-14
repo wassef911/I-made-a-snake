75130cff2b6a43cca147c3f4fc02980311cb3160
Wassef911 <1.wassef911@gmail.com>
2022-10-14 12:54:30 +0100

    chore: last touch before tests

--- src/enum.py
+++ src/enum.py
@@ -1,5 +1,5 @@
 class Parameters:
-    WIDTH = 1500
-    HEIGHT = 1500
-    DELAY = 40  # Milliseconds
-    FOOD_SIZE = 20  # The food square dimension
+    WIDTH = 1000
+    HEIGHT = 1000
+    DELAY = 60  # Milliseconds
+    FOOD_SIZE = 50  # The food square dimension
--- src/helpers.py
+++ src/helpers.py
@@ -1,3 +1,7 @@
+import random
+
+from src.enum import Parameters
+
 def get_distance(pos1, pos2):
     """
         Calculate the distance between the two positions and returns the distance
@@ -8,3 +12,18 @@ def get_distance(pos1, pos2):
     x2, y2 = pos2
     distance = ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** 0.5
     return distance
+
+def get_random_food_position():
+    """
+    Create a random position of food.
+    Returns (x,y) the food position. Do not forget to consider FOOD_SIZE
+    """
+    x = random.randint(
+        -Parameters.WIDTH / 2 + Parameters.FOOD_SIZE,
+        Parameters.WIDTH / 2 - Parameters.FOOD_SIZE,
+    )
+    y = random.randint(
+        -Parameters.HEIGHT / 2 + Parameters.FOOD_SIZE,
+        Parameters.HEIGHT / 2 - Parameters.FOOD_SIZE,
+    )
+    return (x, y)
