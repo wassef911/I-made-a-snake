d820a39e130e014c38e3d4f9b82437fe4fad9dfd
Wassef911 <1.wassef911@gmail.com>
2022-10-14 12:54:15 +0100

    fix: food collision

--- src/classes/generic.py
+++ src/classes/generic.py
@@ -1,11 +1,10 @@
 import turtle
 
-
 class GenericSnakeGame:
+    score = 0
     food = turtle.Turtle()
     screen = turtle.Screen()
     stamper = turtle.Turtle()
-    snake = [[0, 0], [0, 20], [0, 40], [0, 60], [0, 80]]
-    snake_direction = 'up'
     food_position = [0, 0]
     offsets = {"up": (0, 20), "down": (0, -20), "left": (-20, 0), "right": (20, 0)}
+    max_accuracy = 20
